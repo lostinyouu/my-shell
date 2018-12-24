2d6130a2a46d26afbc8dd85ce2df394d4add3781
Lostinyou <954630688@qq.com>
2018-12-24 23:07:56 +0800

    增加 vim,加入文件

--- oh-my-zsh.sh
+++ oh-my-zsh.sh
@@ -1,6 +1,13 @@
 #！/bin/bash
+yum install -y vim
+
 yum install -y zsh
+
 yum install -y git
+
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -e "/66/a zsh-autosuggestions" -i test.txt
 chsh -s /bin/zsh
