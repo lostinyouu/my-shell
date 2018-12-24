5515cdf5ec08236aaada57be916fc0dbaa931a8d
Lostinyou <954630688@qq.com>
2018-12-25 00:18:40 +0800

    docker sh

---
+++ install-docker.sh
@@ -0,0 +1,34 @@
+#！/bin/bash
+
+# 安装 docker
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine
+
+
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum install -y docker-ce
+
+sudo systemctl start docker
+
+# 安装 docker-composer
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker-compose --version
